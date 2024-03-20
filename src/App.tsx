efa480571020b64c6dcc4b23da848aef4cbad2de
Navjeevan Alone <navjeevanalone352@gmail.com>
2024-03-20 16:14:19 +0530

    usereducer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './styles/App.css'
 
-function App() {
-  const [counter, setCounter] = useState(0)
-  function handleIncrement() {
-    setCounter(counter + 1)
-  }
-  function handleDecrement() {
-    setCounter(counter - 1)
-  }
-  function handleReset() {
-    setCounter(0)
-  }
-  return (
-    <div className='flex gap-2 justify-center align-middle'>
-      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={handleDecrement}>-</button>
-      <span className='text-4xl font-bold '>{counter}</span>
-      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={handleIncrement}>+</button>
-  
-      <button className='px-4 py-2 text-white bg-black m-2 hover:bg-slate-900 rounded-md' onClick={handleReset}>reset</button>
+const ACTION = {
+  INCREMENT: 'INCREMENT',
+  INCREMENT_FIVE: 'INCREMENT_FIVE',
+  DECREMENT: 'DECREMENT',
+  DECREMENT_FIVE: 'DECREMENT_FIVE',
+  RESET: 'RESET'
+};
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case ACTION.INCREMENT_FIVE:
+      return { ...state, count: state.count + 5 };
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case ACTION.DECREMENT_FIVE:
+      return { ...state, count: state.count - 5 };
+    case ACTION.RESET:
+      return { ...state, count: 0 };
+    default:
+      console.log("Default case run");
+      return state;
+  }
+};
+
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  return (
+    <div className='flex gap-2 justify-center items-center'>
+      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={() => dispatch({ type: ACTION.DECREMENT_FIVE })}>-5</button>
+      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
+      <span className='text-4xl font-bold'>{state.count}</span>
+      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
+      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={() => dispatch({ type: ACTION.INCREMENT_FIVE })}>+5</button>
+      <button className='px-4 py-2 text-white bg-black mx-2 hover:bg-slate-900 rounded-md' onClick={() => dispatch({ type: ACTION.RESET })}>Reset</button>
     </div>
+
   )
 }
 
